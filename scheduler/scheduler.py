dc01153ea5e1f98e6178f47d362facf8c592d2b4
v-lohvyniuk <volodymyr.lohvyniuk@gmail.com>
2020-06-01 00:20:35 +0300

    Add sceduling 10:00 - 22:00

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,27 +1,20 @@
 import schedule, time, threading
-import datetime
 
 
 class Scheduler:
 
     def __init__(self):
         self.mins = 1
+        self.time = "10:30"
 
     def every_mins(self, mins):
         self.mins = mins
         return self
 
-    @staticmethod
-    def __get_now():
-        return datetime.datetime.now()
-
-    @staticmethod
-    def get_todays_10_AM():
-        return Scheduler.__get_now().replace(hour=10, minute=0, second=0, microsecond=0)
-
-    @staticmethod
-    def get_todays_10_PM():
-        return Scheduler.__get_now().replace(hour=22, minute=0, second=0, microsecond=0)
+#   time like 10:30
+    def every_day_at(self, time):
+        self.time = time
+        return self
 
     def execute(self, job):
         schedule.every(self.mins).minutes.do(job)
@@ -29,8 +22,7 @@ class Scheduler:
 
     def __loop(self):
         while True:
-            if Scheduler.get_todays_10_AM() < Scheduler.__get_now() < Scheduler.get_todays_10_PM():
-                schedule.run_pending()
+            schedule.run_pending()
             time.sleep(1)
 
     def start(self):
