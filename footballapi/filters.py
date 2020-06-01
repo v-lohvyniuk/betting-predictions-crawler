1cc269f68b327757cd702b2cd74f1e0dd2e34fe5
v-lohvyniuk <volodymyr.lohvyniuk@gmail.com>
2020-06-01 23:07:11 +0300

    Fix team filtering

--- footballapi/filters.py
+++ footballapi/filters.py
@@ -9,14 +9,20 @@ class FixtureFilter:
             home_team = fixture.home_team_name.upper()
             away_team = fixture.away_team_name.upper()
 
-            if name_upper in home_team or name_upper in away_team:
+            if name_upper in home_team or\
+                    name_upper in away_team or\
+                    home_team in name_upper or\
+                    away_team in name_upper:
                 results.append(fixture)
 
         return results
 
     @staticmethod
     def filter_by_team_names(fixtures, name1, name2):
+        result_list = []
         fixtures_by_first_team = FixtureFilter.filter_by_team_name(fixtures, name1)
-        fixtures_by_both_teams = FixtureFilter.filter_by_team_name(fixtures_by_first_team, name2)
+        fixtures_by_second_teams = FixtureFilter.filter_by_team_name(fixtures, name2)
 
-        return fixtures_by_both_teams
+        result_list.append(fixtures_by_first_team)
+        result_list.append(fixtures_by_second_teams)
+        return result_list
