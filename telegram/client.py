fbc2dd026882dc8c36d16ef41d74c2c8590aab59
v-lohvyniuk <volodymyr.lohvyniuk@gmail.com>
2020-06-06 01:33:47 +0300

    Fix format

--- telegram/client.py
+++ telegram/client.py
@@ -89,8 +89,8 @@ class TelegramBotService:
     def format_predictions_result_message(predictions):
         message = f"Predictions: {str(len(predictions))}\n"
         for prediction in predictions:
-            message += prediction.__str__() + "\n"
-            message += "----------------"
+            message += prediction.__str__()
+            message += "\n----------------\n"
         return message
 
     def send_default_failure_message(self, chat_id):
